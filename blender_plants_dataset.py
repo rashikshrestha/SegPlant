4bb5e62960dae95a6290303245138e1f5497e38c
rashikshrestha <rashikshrestha01@gmail.com>
2024-10-06 16:19:20 -0400

    option to not get mask

--- blender_plants_dataset.py
+++ blender_plants_dataset.py
@@ -9,12 +9,14 @@ from torchvision.transforms.functional import pil_to_tensor
 class BlenderPlantsDataset(Dataset):
     """
     Wrapper to BlenderPlants dataset.
-    It has 5644 high quality renders of a blender plant from random viewpoint and camera focal length.
-    Among which first 4516 (80%) is the Train split, and last 1128 (20%) is the Evaluation split.
+    It has 5637 high quality renders of a blender plant from random viewpoint and camera focal length.
+    Among which first 4510 (80%) is the Train split, and last 1127 (20%) is the Evaluation split.
     """
-    def __init__(self, path, split='train'):
+    def __init__(self, path, split='train', resize=None, get_mask=True):
       self.path = Path(path)
-      self.idx = np.arange(0,4515,1) if split == 'train' else np.arange(4516, 5644)
+      self.resize = resize
+      self.get_mask = get_mask
+      self.idx = np.arange(0,4510) if split == 'train' else np.arange(4510, 5637)
       # self.idx = np.arange(0,45,1) if split == 'train' else np.arange(4501, 4600)
         
     def __len__(self):
@@ -24,13 +26,18 @@ class BlenderPlantsDataset(Dataset):
       # Read Image
       img_path = self.path/'images'/f"{idx:05d}.png"
       img = Image.open(img_path)
+      if self.resize is not None: img = img.resize(self.resize)
       if img.mode != 'RGB': img = img.convert('RGB')
       img = pil_to_tensor(img)/255.0
       img = img.type(torch.float32)
+
+      if not self.get_mask:
+         return img
      
       # Read branch mask
       branch_path = self.path/'masks'/'branch'/f"{idx:05d}_1.png"
       branch = Image.open(branch_path)
+      if self.resize is not None: branch = branch.resize(self.resize)
       branch = branch.convert('L')
       branch = np.array(branch)
       branch = np.where(branch>200, 1.0, 0.0)
@@ -39,7 +46,8 @@ class BlenderPlantsDataset(Dataset):
       # Read leaf mask
       leaf_path = self.path/'masks'/'leaf'/f"{idx:05d}_1.png"
       leaf = Image.open(leaf_path)
-      leaf = branch.convert('L')
+      if self.resize is not None: leaf = leaf.resize(self.resize)
+      leaf = leaf.convert('L')
       leaf = np.array(leaf)
       leaf = np.where(leaf>200, 1.0, 0.0)
       leaf = torch.as_tensor(leaf, dtype=torch.float32)[None]
